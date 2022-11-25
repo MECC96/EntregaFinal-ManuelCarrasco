350eaee8a17b7b736fe7f887ac96c7e3fd95ef7e
Manuel Carrasco <manuelecarrasco96@gmail.com>
2022-11-25 09:15:34 -0300

    modificacion seccion items

--- react-AlBorder/src/App.jsx
+++ react-AlBorder/src/App.jsx
@@ -3,7 +3,8 @@ import Navbar from './Components/Header/Navbar';
 import ItemListContainer from './Components/Header/ItemListContainer';
 import Carousel from './Components/Main/Carousel';
 import Intro from './Components/Main/Intro';
-import Products from './Components/Main/Products';
+import Products from './Components/Main/Products/Products';
+import Clients from './Components/Main/Comments/Clients';
 import Form from './Components/Main/Form';
 import Footer from './Components/Footer/Footer';
 
@@ -15,6 +16,7 @@ const App = () => {
       <Carousel imgUno={{img:"./images/lake.jpg", altUno:"Imagen provisoria de lago"}} imgDos={{img:"./images/mountain1.jpg", altDos:"Imagen provisoria de montaña 1"}} imgTres={{img:"./images/mountain2.jpg", altTres:"Imagen provisoria de montaña 2"}}/>
       <Intro/>
       <Products/>
+      <Clients/>
       <Form/>
       <Footer/>
     </div>
