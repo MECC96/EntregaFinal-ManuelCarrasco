642a21000e79790d4f536a556c64c4afc5e8c0fd
Manuel Carrasco <manuelecarrasco96@gmail.com>
2022-12-07 09:19:36 -0300

    Eliminando componente comentado

--- react-AlBorder/src/App.jsx
+++ react-AlBorder/src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Header/Navbar";
 import ItemListContainer from "./Components/Header/Products/ItemListContainer";
-//import Carousel from './Components/Main/Carousel';
 import Intro from "./Components/Main/Intro";
 import Clients from "./Components/Main/Comments/Clients";
 import Form from "./Components/Main/Form";
@@ -19,7 +18,6 @@ const App = () => {
         tagTres={"Clientes"}
         tagCuatro={"Contáctanos"}
       />
-      {/* <Carousel imgUno={{img:"./images/lake.jpg", altUno:"Imagen provisoria de lago"}} imgDos={{img:"./images/mountain1.jpg", altDos:"Imagen provisoria de montaña 1"}} imgTres={{img:"./images/mountain2.jpg", altTres:"Imagen provisoria de montaña 2"}}/> */}
       <Routes>
         <Route path={"/"} element={<ItemListContainer />} />
         <Route path={"/category/:id"} element={<ItemListContainer />} />
