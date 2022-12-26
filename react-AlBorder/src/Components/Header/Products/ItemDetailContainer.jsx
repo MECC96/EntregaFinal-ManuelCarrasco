83a3c473a61eefdc2bd8e416595913fb11c9ca09
Manuel Carrasco <manuelecarrasco96@gmail.com>
2022-12-26 10:49:31 -0300

    agregando loader

--- react-AlBorder/src/Components/Header/Products/ItemDetailContainer.jsx
+++ react-AlBorder/src/Components/Header/Products/ItemDetailContainer.jsx
@@ -1,24 +1,26 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Loader from "../../../Loader";
 import { customFetch } from "../../../utils/customFetch";
 import { products } from "../../../utils/products";
 import ItemDetail from "./ItemDetail";
 
 const ItemDetailContainer = () => {
+  const [loading, setLoading] = useState(true)
   const [item, setItem] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
-    customFetch(products.find((ele) => ele.id === parseInt(id))).then(
-      (data) => {
-        setItem(data);
-      }
-    );
+    customFetch(
+      products.find((ele) => ele.id === parseInt(id))
+    , setLoading).then((data) => {
+      setItem(data);
+    });
   }, [id]);
 
   return (
     <div className="container my-5">
-      <ItemDetail item={item} />
+      {loading ? <Loader/> : <ItemDetail item={item} />}
     </div>
   );
 };
--- react-AlBorder/src/Components/Header/Products/ItemListContainer.jsx
+++ react-AlBorder/src/Components/Header/Products/ItemListContainer.jsx
@@ -1,26 +1,29 @@
 import { useState, useEffect } from "react";
 import { products } from "../../../utils/products";
 import { customFetch } from "../../../utils/customFetch";
-import ItemList from "./ItemList";
 import { useParams } from "react-router-dom";
+import ItemList from "./ItemList";
+import Loader from "../../../Loader";
 
 const ItemListContainer = () => {
+  const [loading, setLoading] = useState(true)
   const [items, setItems] = useState([]);
   const { id } = useParams();
 
   useEffect(() => {
     customFetch(
       id ? products.filter((item) => item.category === id) : products
-    ).then((data) => {
+    , setLoading).then((data) => {
       setItems(data);
     });
   }, [id]);
 
   return (
     <div className="container">
-      <ItemList item={items} />
+      {loading ? <Loader/> : <ItemList item={items} />}
     </div>
   );
+  
 };
 
 export default ItemListContainer;
