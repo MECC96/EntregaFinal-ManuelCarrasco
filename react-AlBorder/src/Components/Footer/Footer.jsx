cb21eca048d8e0ee8f3d11fb1fc60a4fb8417c81
Manuel Carrasco <manuelecarrasco96@gmail.com>
2023-01-11 13:44:10 -0300

    Añadiendo iconos de redes sociales

--- react-AlBorder/src/Components/Footer/Footer.jsx
+++ react-AlBorder/src/Components/Footer/Footer.jsx
@@ -1,16 +1,36 @@
+import { Link } from "react-router-dom"
 
 const Footer = () => {
     return (
-        <div className="container-fluid header p-5">
+        <div className="container-fluid header py-2">
             <div className="row">
                 <div className="col-md-4 d-flex align-items-center justify-content-center">
-                    Footer
+                    <div className="col-md-7">
+                        <ul className="nav">
+                            <li className="nav-item">
+                                <a className="nav-link text-muted" href="/"  rel="noreferrer">🐶 Contacto</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-muted" href="/"  rel="noreferrer">🐶 Política de Privacidad</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link text-muted" href="/"  rel="noreferrer">🐶 Trabaja con Nosotros</a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
                 <div className="col-md-4">
-                <img src="../images/border-logo.png" alt="AlBorder logo" width={250}/>
+                    <Link to={"/"}>
+                        <img src="../images/border-logo.png" alt="AlBorder logo" width={250} title={"Home"}/>
+                    </Link>
                 </div>
                 <div className="col-md-4 d-flex align-items-center justify-content-center">
-                    Footer
+                    <div className="col-md-6">
+                        <a href="/" className="mx-2"><img src={"/images/facebook.svg"} alt={"Facebook"} width={30} rel="noreferrer" title={"Facebook"}/></a>
+                        <a href="/" className="mx-2"><img src={"/images/instagram.svg"} alt={"Instagram"} width={30} rel="noreferrer" title={"Instagram"}/></a>
+                        <a href="/" className="mx-2"><img src={"/images/twitter.svg"} alt={"Twitter"} width={30} rel="noreferrer" title={"Twitter"}/></a>
+                        <a href="/" className="mx-2"><img src={"/images/whatsapp.svg"} alt={"Whatsapp"} width={30} rel="noreferrer" title={"Whatsapp"}/></a>
+                    </div>
                 </div>
             </div>
         </div>
