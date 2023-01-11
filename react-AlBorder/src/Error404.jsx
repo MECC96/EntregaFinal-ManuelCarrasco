8c92a68d3080c380ae8d73b0fb0381b4629ba48a
Manuel Carrasco <manuelecarrasco96@gmail.com>
2023-01-11 13:34:55 -0300

    Adición de mensaje de error

--- react-AlBorder/src/Error404.jsx
+++ react-AlBorder/src/Error404.jsx
@@ -1,7 +1,12 @@
 
 const Error404 = () => {
     return (
-        <h2>Error 404!</h2>
+        <div className="row d-flex align-items-center justify-content-center">
+            <div className="col-md-10 alert alert-danger text-center" role="alert">
+                <b>¡Error 404!</b>
+                <p><b>¡Esta página no existe!</b></p>
+            </div>
+        </div>
     )
 }
 
