f61a6354e3c3356cdf9546807a4b28976869a14e
Manuel Carrasco <manuelecarrasco96@gmail.com>
2023-01-11 13:44:58 -0300

    Añadiendo espaciado y link a imagen de producto

--- react-AlBorder/src/Components/Main/Products/Item.jsx
+++ react-AlBorder/src/Components/Main/Products/Item.jsx
@@ -4,15 +4,19 @@ const Item = ({ ele }) => {
   const { image, name, price } = ele;
 
   return (
-    <div className="col-md-3 my-5">
+    <div className="col-md-4 my-5">
       <div className="card shadow m-2 py-5 rounded flex-column">
-        <img src={image} className="card-image-top mt-2" alt={name} />
+        <Link to={"/item/" + ele.id} title="Ver descripción">
+          <div className="d-flex align-items-center justify-content-center" width={10}>
+            <img src={image} className="card-image-top" alt={name} width={200}/>
+          </div>
+        </Link>
         <div className="card-body text-center">
-          <Link to={"/item/" + ele.id} className="text-dark">
-            <h6 className="card-title">{name}</h6>
+          <Link to={"/item/" + ele.id} className="text-dark" title="Ver descripción">
+            <h6 className="card-title fs-5">{name}</h6>
           </Link>
           <p>
-            <span>${price}</span>
+            <span className="fs-4">${price}</span>
           </p>
         </div>
       </div>
