1b6fb6dba7992468d2324c96190beefaefecf9bc
Manuel Carrasco <manuelecarrasco96@gmail.com>
2023-01-11 13:46:11 -0300

    Agregado de render condicional al icono carrito

--- react-AlBorder/src/Components/Header/CartWidget.jsx
+++ react-AlBorder/src/Components/Header/CartWidget.jsx
@@ -2,16 +2,21 @@ import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { CartContext } from "../Context/CartContext";
 
-const CartWidget = () => {
-    const {cartTotal} = useContext(CartContext);
+const Badge = ({itemsTotal})=>{
     return (
         <div>
             <Link to={"/cart"} className="btn cart position-relative">
                 <img src="../images/cart-check.svg" alt="Cart" title="Cart" width={30}/>
-                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-info">{cartTotal()}</span>
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-info">{itemsTotal}</span>
             </Link>
         </div>
     )
+}
+
+const CartWidget = () => {
+    const {cartTotal} = useContext(CartContext);
+    
+    return cartTotal() > 0 ? <Badge itemsTotal={cartTotal()}/> : "";
 }
 
 export default CartWidget
