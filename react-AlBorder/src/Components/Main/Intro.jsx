bc667cfcfad981613ed9bbffa052d0120d2a1693
Manuel Carrasco <manuelecarrasco96@gmail.com>
2023-01-13 09:59:08 -0300

    Agregando estilo foto collies

--- react-AlBorder/src/Components/Main/Intro.jsx
+++ react-AlBorder/src/Components/Main/Intro.jsx
@@ -6,11 +6,11 @@ const Intro = () => {
             <h3 className='text-center p-3 my-3'>Quiénes Somos</h3>
             <div className="row d-flex justify-content-center">
                 <div className="col-md-10 d-flex justify-content-between header shadow m-3 rounded py-5">
-                    <div className="col-md-6 d-flex justify-content-center">
-                        <img src="./images/borders.jfif" alt="Border Collies" />
+                    <div className="col-md-6 d-flex justify-content-center ">
+                        <img src="./images/borders.jfif" className="dogs" alt="Border Collies" title={"Boder Collies"} />
                     </div>
                     <div className="col-md-6 d-flex justify-content-center align-items-center px-5">
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, soluta nulla esse accusamus vel deleniti accusantium unde doloribus voluptas asperiores fuga dolor obcaecati explicabo ut minima exercitationem, at ex doloremque.</p>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique nobis rerum itaque! Obcaecati eveniet perspiciatis blanditiis consequatur minima consectetur! Voluptas corrupti ipsum labore nulla suscipit magnam aliquam ex animi ipsa!</p>
                     </div>
             </div>
         </div>
