cb855548d31549409f93894cbe8130e8cbaf8d5c
Manuel Carrasco <manuelecarrasco96@gmail.com>
2023-01-13 10:41:35 -0300

    Eliminación de productos y del simulador de fetch

--- react-AlBorder/src/utils/customFetch.js
+++
@@ -1,8 +0,0 @@
-export const customFetch = (products, setLoading) =>{
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            setLoading(false)
-            resolve(products)
-        }, 3000);
-    })
-}
